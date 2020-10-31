1cbd8ce47c84e10de1df24449f3d6992249a1874
Vinicius Imai <vinicius.imai@gmail.com>
2020-10-30 21:09:48 -0300

    first example endpoint

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ async def root():
     return {"message": "Hello World"}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print('PyCharm')
+@app.get("/documentos}")
+async def read_documento():
+    return [{"documento_uuid": "uuid1"}, {"documento_uuid": "uuid1"}]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.get("/documentos/{documento_uuid}")
+async def read_documento(documento_uuid):
+    return {"documento_uuid": documento_uuid}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
