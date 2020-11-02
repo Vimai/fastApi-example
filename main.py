e5eb79188552559b0b26041a8a1ab04e2b957618
Vinicius Imai <vinicius.imai@gmail.com>
2020-11-01 21:10:12 -0300

    query param

--- main.py
+++ main.py
@@ -21,3 +21,11 @@ async def read_documento(documento_uuid):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
