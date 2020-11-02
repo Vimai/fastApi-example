f2b61deb84bc3387694b0208f60fb4159fa6bb0a
Vinicius Imai <vinicius.imai@gmail.com>
2020-11-02 20:08:45 -0300

    request body

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from fastapi import FastAPI
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -29,3 +32,17 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip + limit]
+
+
+# Request body
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None  # optional
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
