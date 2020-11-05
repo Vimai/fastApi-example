121923149da4fdaf62e9840e3494769f537f1c5b
Vinicius Imai <vinicius.imai@gmail.com>
2020-11-04 21:04:16 -0300

    poc pymongo

---
+++ mongo_repository.py
@@ -0,0 +1,59 @@
+from pymongo import MongoClient
+
+
+class MongoRepository:
+    def __init__(self):
+        self.client = MongoClient(host='localhost',
+                              port=27017,
+                              username='root',
+                              password='password')
+        self.db = self.client.projectdb
+
+    def create_product(self, payload):
+        products = self.db.products
+        products.insert_one(payload)
+        
+
+client = MongoClient(host='localhost',
+                     port=27017,
+                     username='root',
+                     password='password')
+db = client.projectdb
+posts = db.posts
+
+post = {"author": "Mike",
+        "text": "My first blog post!"}
+
+post2 = {"author": "Mike2",
+         "text": "My first blog post!2"}
+
+post3 = {"author": "Mike3",
+         "text": "My first blog post!3"}
+
+post_id = posts.insert_one(post).inserted_id
+
+posts.insert_many([post3, post2])
+
+print(db)
+print(db.list_collection_names())
+
+results = posts.find({"author": "Mike3"})
+print(results)
+print('em lop: \n')
+for result in results:
+    print(f"result: {result} and id: {result['_id']}")
+
+posts.update_one({"author": "Mike3"}, {"$set": {"author": "autor 3"}})
+
+results = posts.find({})
+print(results)
+print('em lop 2: \n')
+for result in results:
+    print(f"result: {result} and id: {result['_id']}")
+    
+    
+breakpoint()
+
+
+
+
