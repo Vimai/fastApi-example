32bc444f4b2f68bebe2101597c5e40cc62d68f89
Vinicius Imai <vinicius.imai@gmail.com>
2020-11-07 19:02:54 -0300

    Use tortoise and sqlite

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ from fastapi import FastAPI, Header, HTTPException
 
 from pydantic import BaseModel
 
+from tortoise import fields
+from tortoise.models import Model
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+import requests
+
 app = FastAPI()
 
 
-class City(BaseModel):
-    name: str
-    timezone: str
+class City(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(50, unique=True)
+    timezone = fields.CharField(50)
 
+    def current_time(self) -> str:
+        r = requests.get(f'http://worldtimeapi.org/api/timezone/{self.timezone}')
+        response = r.json()
+        current_time = response.get('datetime') or ''
+        return current_time
+
+    class PydanticMeta:
+        computed = ('current_time', )
+
+
+City_Paydantic = pydantic_model_creator(City, name='City')
+CityIn_Paydantic = pydantic_model_creator(City, name='CityIn', exclude_readonly=True)
 
 db = []
 
@@ -21,21 +41,30 @@ async def root():
 
 @app.get("/cities")
 async def get_cities():
-    return db
+    response = await City_Paydantic.from_queryset(City.all())
+    return response
 
 
 @app.get("/cities/{city_id}")
 async def read_item(city_id: int):
-    return db[city_id - 1]
+    return await City_Paydantic.from_queryset_single(City.get(id=city_id))
 
 
 @app.post("/cities/")
-async def create_city(city: City):
-    db.append(city.dict())
-    return db[-1]
+async def create_city(city: CityIn_Paydantic):
+    city_obj = await City.create(**city.dict(exclude_unset=True))
+    return await City_Paydantic.from_tortoise_orm(city_obj)
 
 
 @app.delete("/cities/{city_id}")
 async def delete_city(city_id: int):
-    db.pop(city_id - 1)
+    await City.filter(id=city_id).delete()
     return {}
+
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
