6298e7fb7a6efe36dc3b20b03bbef6dbeb21140f
Vinicius Imai <vinicius.imai@gmail.com>
2020-11-09 21:21:10 -0300

    basic read users

--- routers/users.py
+++ routers/users.py
@@ -1,11 +1,29 @@
+from typing import Optional
 from fastapi import APIRouter
+
+from pydantic import BaseModel
+from pymongo import MongoClient
+
 
 router = APIRouter()
 
 
+class Users(BaseModel):
+    name: str
+    age: int
+    city: Optional[str] = None  # optional
+
+
 @router.get("/users/", tags=["users"])
 async def read_users():
-    return [{"username": "Foo"}, {"username": "Bar"}]
+    client = MongoClient(host='localhost',
+                              port=27017,
+                              username='root',
+                              password='password')
+    db = client.projectdb
+    users = db.users
+    results = users.find({})
+    return results
 
 
 @router.get("/users/me", tags=["users"])
